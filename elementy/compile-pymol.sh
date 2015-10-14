173c17bfa01c1d106b1c518b6dc53a6955369a90
filips <stefaniak@gmail.com>
2015-10-14 11:13:46 +0200

    Create compile-pymol.sh

---
+++ elementy/compile-pymol.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+prefix=/opt/pymol-svn
+modules=$prefix/modules
+
+# enable c++11
+export CPPFLAGS="-std=c++0x"
+
+# If you want to install as root, then split this line up in "build"
+# and "install" and run the "install" with "sudo"
+sudo python setup.py build install \
+    --home=$prefix \
+    --install-lib=$modules \
+    --install-scripts=$prefix
